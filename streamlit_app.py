92d133e80aa40f950951fb4b6603271b7d37b73b
lmgibson <landon.g42@gmail.com>
2020-11-01 08:23:02 -0800

    Work on streamlit app. Updated web app to include information on the average hourly rate by skill category. Note, this assumes that no user enters a skill more than once, otherwise the calculated average would misrepresent the true average. However, there is no reason to believe a user is capable of entering a skill more than once given the user interface.

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,11 +15,31 @@ api_key = os.environ['AIRTABLE_API_KEY']
 base_key = os.environ['AIRTABLE_FREELANCE_BASE_KEY']
 table_name = 'freelancers'
 airtable = Airtable(base_key, table_name, api_key)
-dt = airtable.get_all(maxRecords=20)
+dt = airtable.get_all()
 df = pd.DataFrame.from_records((r['fields'] for r in dt))
+
+
+# Functions
+def string_to_list(series):
+    stripped_skills_list = series.str.strip('][')
+    split_skills_list = stripped_skills_list.str.split(', ')
+    return split_skills_list
+
+
+def rate_by_skill(data):
+    exploded_df = data.explode('skills_list')
+    results = exploded_df.groupby(['skills_list'])[
+        'hourly_rate'].agg(['mean', 'count'])
+    return results.sort_values(by=['count'], ascending=False)
+
+
+# Cleaning df
+df['skills_list'] = string_to_list(df['skills_list'])
+
 
 # Building web app
 st.title("Freelance Hourly Rate Data")
 st.markdown(
     "Welcome! This app provides data and trends of freelancer hourly rates.")
 st.write(df.groupby(['date_accessed'])['hourly_rate'].mean())
+st.write(rate_by_skill(df))
