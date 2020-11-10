1e870099a429ca0ae509f221894be7d56c327cb5
lmgibson <landon.g42@gmail.com>
2020-11-09 17:40:47 -0800

    Cleaning and debugging streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,59 +1,59 @@
-# Reference: https://docs.streamlit.io/en/latest/getting_started.html
-# Reference for hosting: https://towardsdatascience.com/how-to-deploy-a-streamlit-app-using-an-amazon-free-ec2-instance-416a41f69dc3
-# Importing Libraries
 from airtable import Airtable
-import streamlit as st  # creating web-app
-import pandas as pd  # managing data
-import numpy as np  # managing data for model
+import streamlit as st
+import pandas as pd
+import numpy as np
 import os
 from datetime import date
 
-################################################
+
+def loadDatafromAirtable():
+    api_key = os.environ['AIRTABLE_API_KEY']
+    base_key = os.environ['AIRTABLE_FREELANCE_BASE_KEY']
+    table_name = 'freelancers'
+    airtable = Airtable(base_key, table_name, api_key)
+    return pd.DataFrame.from_records(
+        (r['fields'] for r in airtable.get_all()))
 
 
-# Accessing API
-api_key = os.environ['AIRTABLE_API_KEY']
-base_key = os.environ['AIRTABLE_FREELANCE_BASE_KEY']
-table_name = 'freelancers'
-airtable = Airtable(base_key, table_name, api_key)
-dt = airtable.get_all()
-df = pd.DataFrame.from_records((r['fields'] for r in dt))
+def calculateOverallHourlyRate():
+    return pandasData.groupby(['date_accessed'])['hourly_rate'].mean()
 
 
-# Functions
-def string_to_list(series):
-    stripped_skills_list = series.str.strip('][')
-    split_skills_list = stripped_skills_list.str.split(', ')
-    return split_skills_list
+def convertSkillsStringToList():
+    strippedSkillsList = pandasData['skills_list'].str.strip('][')
+    splitSkillsList = strippedSkillsList.str.split(', ')
+    return splitSkillsList
 
 
-def rate_by_skill(data):
+def filterPandasDataToMostRecentDate():
     today = date.today()
-    data = data[(data['date_accessed'] == str(today))]
-    exploded_df = data.explode('skills_list')
-    results = exploded_df.groupby(['skills_list'])[
+    return pandasData[(pandasData['date_accessed'] == str(today))]
+
+
+def calculateHourlyRatebySkill():
+    pandasData = filterPandasDataToMostRecentDate()
+    pandasData['skills_list'] = convertSkillsStringToList()
+    explodedPandasData = pandasData.explode('skills_list')
+    results = explodedPandasData.groupby(['skills_list'])[
         'hourly_rate'].agg(['mean', 'count'])
     return results.sort_values(by=['count', 'mean'], ascending=False)
 
 
-# Cleaning df
-df['skills_list'] = string_to_list(df['skills_list'])
-
-
-# Building web app
 st.title("Freelance Hourly Rate Trends")
 st.markdown(
     "Welcome! This app provides data and trends of freelancer hourly rates.")
 st.markdown(
     "## Trends in overall average hourly rate"
 )
-st.write(df.groupby(['date_accessed'])['hourly_rate'].mean())
+pandasData = loadDatafromAirtable()
+st.write(calculateOverallHourlyRate())
 
+hourlyRatesBySkill = calculateHourlyRatebySkill()
 col1, col2 = st.beta_columns(2)
 col1.markdown("## Most popular skills")
-col1.write(rate_by_skill(df)['count'].head(5))
+col1.write(hourlyRatesBySkill.head(5))
 col2.markdown("## Least popular skills")
-col2.write(rate_by_skill(df)['count'].tail(5))
+col2.write(hourlyRatesBySkill.tail(5))
 
 st.markdown("## Average hourly rate by skill category")
-st.write(rate_by_skill(df))
+st.write(hourlyRatesBySkill)
