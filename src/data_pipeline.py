6a5f88358cfc1a50d25408e0c20c37d6f37b676b
lmgibson <landon.g42@gmail.com>
2021-02-02 21:39:41 -0800

    Added function to import rates into a rate table

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -4,19 +4,20 @@ from freelance_scrape import clean as cl
 from freelance_scrape import importPgSQL as imp
 
 # Scraping Static Elements
-scraper = ss.GuruScraper()
-scraper.generate_urls(startPage=1, endPage=10)
-scraper.html_extract()
-scraper.freelancer_extraction()
-scraper.data_extraction()
+# scraper = ss.GuruScraper()
+# scraper.generate_urls(startPage=1, endPage=10)
+# scraper.html_extract()
+# scraper.freelancer_extraction()
+# scraper.data_extraction()
 
-# Cleaning data
-clean_class = cl.CleanData()
-clean_class.merge_data()
+# # Cleaning data
+# clean_class = cl.CleanData()
+# clean_class.merge_data()
 
-print("Scrape and Clean complete")
+# print("Scrape and Clean complete")
 
 # Insert into postgres db
 imp.insertUsers()
+imp.insertRates()
 
 print("Upload complete!")
--- src/freelance_scrape/importPgSQL.py
+++ src/freelance_scrape/importPgSQL.py
@@ -2,16 +2,42 @@ import csv
 import psycopg2
 from datetime import date
 
+conn = psycopg2.connect("host=localhost dbname=testdb user=Metaverse")
+cur = conn.cursor()
+today = date.today().strftime("%d%m%Y")
+filename = "./data/processed/user_data_" + today + ".csv"
+with open(filename) as f:
+    reader = csv.reader(f)
+    next(reader)
+    for row in reader:
+        pass
+
 
 def insertUsers():
     conn = psycopg2.connect("host=localhost dbname=testdb user=Metaverse")
     cur = conn.cursor()
     today = date.today().strftime("%d%m%Y")
-    filename = "./data/processed/user_data_" + today + ".json"
+    filename = "./data/processed/user_data_" + today + ".csv"
     with open(filename) as f:
         reader = csv.reader(f)
         next(reader)
         for row in reader:
             cur.execute(
-                "INSERT INTO users VALUES (Default, %s) ON CONFLICT DO NOTHING/UPDATE", (row[1],))
+                "INSERT INTO users VALUES (Default, %s) ON CONFLICT (name) DO NOTHING",
+                (row[1],))
+    conn.commit()
+
+
+def insertRates():
+    conn = psycopg2.connect("host=localhost dbname=testdb user=Metaverse")
+    cur = conn.cursor()
+    today = date.today().strftime("%d%m%Y")
+    filename = "./data/processed/user_data_" + today + ".csv"
+    with open(filename) as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            cur.execute(
+                "INSERT INTO rates VALUES (%s, %s, %s) ON CONFLICT (name, scrapeDate) DO NOTHING",
+                (row[1:4]))
     conn.commit()
