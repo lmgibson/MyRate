dae9b4cfb954f50d5bf6a74a0c10142eb4999429
lmgibson <landon.g42@gmail.com>
2021-02-02 20:47:16 -0800

    Added function to insert users from csv.

--- src/freelance_scrape/importPgSQL.py
+++ src/freelance_scrape/importPgSQL.py
@@ -1,1 +1,13 @@
+import csv
 import psycopg2
+
+
+def insertUsers(data):
+    conn = psycopg2.connect("host=localhost dbname=testdb user=Metaverse")
+    cur = conn.cursor()
+    with open("./data/processed/user_data_01112020.csv") as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            cur.execute("INSERT INTO users VALUES (Default, %s)", (row[1],))
+    conn.commit()
